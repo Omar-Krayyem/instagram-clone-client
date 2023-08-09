503ea86cf8f15155181eae68465b3c0435ed314c
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-08-09 22:35:07 +0300

    Created addpost component <react>

---
+++ src/components/addPost/addPost.jsx
@@ -0,0 +1,35 @@
+import "../addPost/addPost.css"
+import PermMediaOutlinedIcon from '@mui/icons-material/PermMediaOutlined';
+import CloseIcon from '@mui/icons-material/Close';
+function AddPost() {
+    return (
+        <div className="create-post">
+            <div className="close">
+                <CloseIcon className="close_icon"/>
+            </div>
+            <div className="box">
+                <div className="title">
+                    <div className="icon">
+                    </div>
+                    <div>Create new Post</div>
+                    <div className="back-icon icon">
+                        <button>Share</button>
+                    </div>
+                </div>
+                <div className="bottom">
+                    <div className="content">
+                        <div className="icon">
+                            <PermMediaOutlinedIcon className="icon"/>
+                        </div>
+                        <h3>Drag photos and videos here</h3>
+                        <div>
+                            <input type="file" title="Select from computer" placeholder="Select from computer"></input>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default AddPost
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,6 +2,7 @@ import '../home/home.css';
 import Nav from '../../components/nav/nav';
 import Timeline from '../../components/timeline/timeline';
 import Suggestions from "../../components/suggestion/suggestion";
+import AddPost from '../../components/addPost/addPost';
 
 function Home (){
     return (
@@ -11,6 +12,7 @@ function Home (){
             </div>
             <div className="homepage__timeline">
                 <Timeline />
+                <AddPost/>
             </div>
             <div>
                 <Suggestions/>
